0f27aed3d70ce0c9294733d0c6afe5d88c8a4ac2
Pankaj Nandeshwar <106176661+CaptainHades007@users.noreply.github.com>
2024-05-18 17:03:17 +0530

    Update 04_Indore_Prediction_1km_radius_heatmaps.py

--- pages/04_Indore_Prediction_1km_radius_heatmaps.py
+++ pages/04_Indore_Prediction_1km_radius_heatmaps.py
@@ -103,10 +103,100 @@ template = """
     }
 </style>
 {% endmacro %}"""
+template2 = """
+{% macro html(this, kwargs) %}
 
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jQuery UI Draggable - Default functionality</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+  <script>
+  $( function() {
+    $( "#maplegend" ).draggable({
+                    start: function (event, ui) {
+                        $(this).css({
+                            right: "auto",
+                            top: "auto",
+                            bottom: "auto"
+                        });
+                    }
+                });
+});
+
+  </script>
+</head>
+<body>
+
+
+<div id='maplegend' class='maplegend' 
+    style='position: absolute; z-index:9999; border:2px solid grey; background-color:rgba(255, 255, 255, 0.8);
+     border-radius:6px; padding: 10px; font-size:14px; right: 20px; bottom: 20px;'>
+
+<div class='legend-title'>Legend</div>
+<div class='legend-scale'>
+  <ul class='legend-labels'>
+    <li><span style='background:red;opacity:0.7;'></span>APL</li>
+    <li><span style='background:blue;opacity:0.7;'></span>BPL</li>
+
+  </ul>
+</div>
+</div>
+
+</body>
+</html>
+
+<style type='text/css'>
+  .maplegend .legend-title {
+    text-align: left;
+    margin-bottom: 5px;
+    font-weight: bold;
+    font-size: 90%;
+    }
+  .maplegend .legend-scale ul {
+    margin: 0;
+    margin-bottom: 5px;
+    padding: 0;
+    float: left;
+    list-style: none;
+    }
+  .maplegend .legend-scale ul li {
+    font-size: 80%;
+    list-style: none;
+    margin-left: 0;
+    line-height: 18px;
+    margin-bottom: 2px;
+    }
+  .maplegend ul.legend-labels li span {
+    display: block;
+    float: left;
+    height: 16px;
+    width: 30px;
+    margin-right: 5px;
+    margin-left: 0;
+    border: 1px solid #999;
+    }
+  .maplegend .legend-source {
+    font-size: 80%;
+    color: #777;
+    clear: both;
+    }
+  .maplegend a {
+    color: #777;
+    }
+</style>
+{% endmacro %}"""
 # Add MacroElement to HeatMap
 macro = MacroElement()
 macro._template = Template(template)
+macro2 = MacroElement()
+macro2._template = Template(template2)
 # france_map.add_child(macro)
 # france_map.save('urban_trees_density_map.html')
 
@@ -129,7 +219,7 @@ m5=folium.Map(location=[data.latitude.mean(),data.longitude.mean()],zoom_start=9
 map_values = data[['latitude','longitude','label']]
 dat = map_values.values.tolist()
 hm = HeatMap(dat,min_opacity=0.2,max_opacity=0.8,radius = 20).add_to(m5)
-m5.add_child(macro)
+m5.add_child(macro2)
 folium_static(m5, width=650, height=650)
 st.write('Overall heatmap')
 
