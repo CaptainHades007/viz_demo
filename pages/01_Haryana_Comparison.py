43a919b9c26f74d7982d68adc3c59fe4bffe2286
Pankaj Nandeshwar <106176661+CaptainHades007@users.noreply.github.com>
2024-05-18 11:56:26 +0530

    Update 01_Haryana_Comparison.py

--- pages/01_Haryana_Comparison.py
+++ pages/01_Haryana_Comparison.py
@@ -148,29 +148,29 @@ with col2:
     st.write('Actual Classification')
     st.plotly_chart(fig2, use_container_width=True)
 
-m1=folium.Map(location=[data.latitude.mean(),data.longitude.mean()],zoom_start=8,control_scale=True)
-m1.fit_bounds([[27, 74], [31, 79]])
-map_values = data[['latitude','longitude','label']]
-dat = map_values.values.tolist()
-hm = HeatMap(dat,min_opacity=0.2,max_opacity=0.8,radius = 20).add_to(m1)
-# st_folium(m1)
+# m1=folium.Map(location=[data.latitude.mean(),data.longitude.mean()],zoom_start=8,control_scale=True)
+# m1.fit_bounds([[27, 74], [31, 79]])
+# map_values = data[['latitude','longitude','label']]
+# dat = map_values.values.tolist()
+# hm = HeatMap(dat,min_opacity=0.2,max_opacity=0.8,radius = 20).add_to(m1)
+# # st_folium(m1)
 
-m2=folium.Map(location=[data2.latitude.mean(),data2.longitude.mean()],zoom_start=8,control_scale=True)
-m2.fit_bounds([[27, 74], [31, 79]])
-map_values = data2[['latitude','longitude','label']]
-dat2 = map_values.values.tolist()
-hm2 = HeatMap(dat2,min_opacity=0.2,max_opacity=0.8,radius = 20).add_to(m2)
-# st_folium(m2)
+# m2=folium.Map(location=[data2.latitude.mean(),data2.longitude.mean()],zoom_start=8,control_scale=True)
+# m2.fit_bounds([[27, 74], [31, 79]])
+# map_values = data2[['latitude','longitude','label']]
+# dat2 = map_values.values.tolist()
+# hm2 = HeatMap(dat2,min_opacity=0.2,max_opacity=0.8,radius = 20).add_to(m2)
+# # st_folium(m2)
 
-col3, col4 = st.columns(2)
+# col3, col4 = st.columns(2)
 
-with col3:
-    # st_folium(m1, use_container_width=True)
-    folium_static(m1, width=650, height=650)
-    st.write('Predicted Heatmap')
+# with col3:
+#     # st_folium(m1, use_container_width=True)
+#     folium_static(m1, width=650, height=650)
+#     st.write('Predicted Heatmap')
 
-with col4:
-    # st_folium(m2, use_container_width=True)
-    folium_static(m2, width=650, height=650)
-    st.write("Actual Heatmap")
-# Run this with `streamlit run your_script_name.py`
+# with col4:
+#     # st_folium(m2, use_container_width=True)
+#     folium_static(m2, width=650, height=650)
+#     st.write("Actual Heatmap")
+# # Run this with `streamlit run your_script_name.py`
