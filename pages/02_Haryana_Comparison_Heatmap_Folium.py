1a41c3e4418362262d4ca04a43fe30ad73cdff28
Pankaj Nandeshwar <106176661+CaptainHades007@users.noreply.github.com>
2024-05-18 14:25:35 +0530

    Update and rename 02_Haryana_Comparison_Heatmap.py to 02_Haryana_Comparison_Heatmap_Folium.py

--- pages/02_Haryana_Comparison_Heatmap_Folium.py
+++ pages/02_Haryana_Comparison_Heatmap_Folium.py
@@ -60,36 +60,36 @@ with col4:
 # Run this with `streamlit run your_script_name.py`
 
 
-m3=folium.Map(location=[data_1.latitude.mean(),data_1.longitude.mean()],zoom_start=8,control_scale=True)
-# m1.fit_bounds([[27, 74], [31, 79]])
-map_values1 = data_1[['latitude','longitude','label']]
-map_values0 = data_0[['latitude','longitude','label']]
-dat1 = map_values1.values.tolist()
-dat0 = map_values0.values.tolist()
-hm3 = HeatMap(dat1,min_opacity=0.2,max_opacity=0.8,gradient={0.0: 'white',  1.0: 'red'},radius = 25).add_to(m3)
-hm4 = HeatMap(dat0,min_opacity=0.2,max_opacity=0.8,gradient={0.0: 'cyan',  1.0: 'white'},radius = 25).add_to(m3)
-# folium_static(m3, width=650, height=650)
-# st.write("Predicted Heatmap for combined localities")
+# m3=folium.Map(location=[data_1.latitude.mean(),data_1.longitude.mean()],zoom_start=8,control_scale=True)
+# # m1.fit_bounds([[27, 74], [31, 79]])
+# map_values1 = data_1[['latitude','longitude','label']]
+# map_values0 = data_0[['latitude','longitude','label']]
+# dat1 = map_values1.values.tolist()
+# dat0 = map_values0.values.tolist()
+# hm3 = HeatMap(dat1,min_opacity=0.2,max_opacity=0.8,gradient={0.0: 'white',  1.0: 'red'},radius = 25).add_to(m3)
+# hm4 = HeatMap(dat0,min_opacity=0.2,max_opacity=0.8,gradient={0.0: 'cyan',  1.0: 'white'},radius = 25).add_to(m3)
+# # folium_static(m3, width=650, height=650)
+# # st.write("Predicted Heatmap for combined localities")
 
-m4=folium.Map(location=[data2_1.latitude.mean(),data2_1.longitude.mean()],zoom_start=8,control_scale=True)
-# m1.fit_bounds([[27, 74], [31, 79]])
-map_values1 = data2_1[['latitude','longitude','label']]
-map_values0 = data2_0[['latitude','longitude','label']]
-dat1 = map_values1.values.tolist()
-dat0 = map_values0.values.tolist()
-hm3 = HeatMap(dat1,min_opacity=0.2,max_opacity=0.8,gradient={0.0: 'white',  1.0: 'red'},radius = 25).add_to(m4)
-hm4 = HeatMap(dat0,min_opacity=0.2,max_opacity=0.8,gradient={0.0: 'cyan',  1.0: 'white'},radius = 25).add_to(m4)
-# folium_static(m4, width=650, height=650)
-# st.write("actual Heatmap 2 for combined localities")
+# m4=folium.Map(location=[data2_1.latitude.mean(),data2_1.longitude.mean()],zoom_start=8,control_scale=True)
+# # m1.fit_bounds([[27, 74], [31, 79]])
+# map_values1 = data2_1[['latitude','longitude','label']]
+# map_values0 = data2_0[['latitude','longitude','label']]
+# dat1 = map_values1.values.tolist()
+# dat0 = map_values0.values.tolist()
+# hm3 = HeatMap(dat1,min_opacity=0.2,max_opacity=0.8,gradient={0.0: 'white',  1.0: 'red'},radius = 25).add_to(m4)
+# hm4 = HeatMap(dat0,min_opacity=0.2,max_opacity=0.8,gradient={0.0: 'cyan',  1.0: 'white'},radius = 25).add_to(m4)
+# # folium_static(m4, width=650, height=650)
+# # st.write("actual Heatmap 2 for combined localities")
 
-col5, col6 = st.columns(2)
+# col5, col6 = st.columns(2)
 
-with col5:
-    # st_folium(m1, use_container_width=True)
-    folium_static(m3, width=650, height=650)
-    st.write('Predicted Heatmap')
+# with col5:
+#     # st_folium(m1, use_container_width=True)
+#     folium_static(m3, width=650, height=650)
+#     st.write('Predicted Heatmap')
 
-with col6:
-    # st_folium(m2, use_container_width=True)
-    folium_static(m4, width=650, height=650)
-    st.write("Actual Heatmap")
+# with col6:
+#     # st_folium(m2, use_container_width=True)
+#     folium_static(m4, width=650, height=650)
+#     st.write("Actual Heatmap")
