b96f84cf4d3ba081f864cd570d6ca6db8a645b15
Pankaj Nandeshwar <106176661+CaptainHades007@users.noreply.github.com>
2024-05-18 14:21:08 +0530

    Update 02_Haryana_Comparison_Heatmap.py

--- pages/02_Haryana_Comparison_Heatmap.py
+++ pages/02_Haryana_Comparison_Heatmap.py
@@ -27,7 +27,10 @@ data2['class'] = data2['label'].map({0: 'BPL', 1: 'APL'})
 # Streamlit webpage title
 st.title('Haryana model comparison heatmap')
 
-
+data_0 = data[data['label']==0]
+data_1 = data[data['label']==1]
+data2_0 = data2[data2['label']==0]
+data2_1 = data2[data2['label']==1]
 
 m1=folium.Map(location=[data.latitude.mean(),data.longitude.mean()],zoom_start=8,control_scale=True)
 m1.fit_bounds([[27, 74], [31, 79]])
@@ -55,3 +58,26 @@ with col4:
     folium_static(m2, width=650, height=650)
     st.write("Actual Heatmap")
 # Run this with `streamlit run your_script_name.py`
+
+
+m3=folium.Map(location=[data_1.latitude.mean(),data_1.longitude.mean()],zoom_start=8,control_scale=True)
+# m1.fit_bounds([[27, 74], [31, 79]])
+map_values1 = data_1[['latitude','longitude','label']]
+map_values0 = data_0[['latitude','longitude','label']]
+dat1 = map_values1.values.tolist()
+dat0 = map_values0.values.tolist()
+hm3 = HeatMap(dat1,min_opacity=0.2,max_opacity=0.8,gradient={0.0: 'white',  1.0: 'red'},radius = 25).add_to(m3)
+hm4 = HeatMap(dat0,min_opacity=0.2,max_opacity=0.8,gradient={0.0: 'cyan',  1.0: 'white'},radius = 25).add_to(m3)
+folium_static(m3, width=650, height=650)
+st.write("Predicted Heatmap for combined localities")
+
+m4=folium.Map(location=[data2_1.latitude.mean(),data2_1.longitude.mean()],zoom_start=8,control_scale=True)
+# m1.fit_bounds([[27, 74], [31, 79]])
+map_values1 = data2_1[['latitude','longitude','label']]
+map_values0 = data2_0[['latitude','longitude','label']]
+dat1 = map_values1.values.tolist()
+dat0 = map_values0.values.tolist()
+hm3 = HeatMap(dat1,min_opacity=0.2,max_opacity=0.8,gradient={0.0: 'white',  1.0: 'red'},radius = 25).add_to(m4)
+hm4 = HeatMap(dat0,min_opacity=0.2,max_opacity=0.8,gradient={0.0: 'cyan',  1.0: 'white'},radius = 25).add_to(m4)
+folium_static(m4, width=650, height=650)
+st.write("actual Heatmap 2 for combined localities")
