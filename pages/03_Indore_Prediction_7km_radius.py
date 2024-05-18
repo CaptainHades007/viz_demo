849562b2abc0462862aa9995e98600050eba2a02
Pankaj Nandeshwar <106176661+CaptainHades007@users.noreply.github.com>
2024-05-18 14:12:15 +0530

    Update 03_Indore_Prediction_7km_radius.py

--- pages/03_Indore_Prediction_7km_radius.py
+++ pages/03_Indore_Prediction_7km_radius.py
@@ -67,12 +67,12 @@ col3, col4 = st.columns(2)
 
 with col3:
     # st_folium(m1, use_container_width=True)
-    folium_static(m1, width=650, height=650)
+    folium_static(m1, width=550, height=650)
     st.write('Predicted Heatmap for poor localities')
 
 with col4:
     # st_folium(m2, use_container_width=True)
-    folium_static(m2, width=650, height=650)
+    folium_static(m2, width=550, height=650)
     st.write("Predicted Heatmap for rich localities")
 
 m3=folium.Map(location=[data_1.latitude.mean(),data_1.longitude.mean()],zoom_start=8,control_scale=True)
