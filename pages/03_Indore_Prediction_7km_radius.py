ef7243ca7acc85d3708f71c24228350e47e058fb
Pankaj Nandeshwar <106176661+CaptainHades007@users.noreply.github.com>
2024-05-18 13:06:29 +0530

    Update 03_Indore_Prediction_7km_radius.py

--- pages/03_Indore_Prediction_7km_radius.py
+++ pages/03_Indore_Prediction_7km_radius.py
@@ -42,7 +42,7 @@ m1=folium.Map(location=[data_0.latitude.mean(),data_0.longitude.mean()],zoom_sta
 # m1.fit_bounds([[27, 74], [31, 79]])
 map_values = data_0[['latitude','longitude','label']]
 dat = map_values.values.tolist()
-hm = HeatMap(dat,min_opacity=0.5,max_opacity=0.8,gradient={0.0: 'blue',  1.0: 'white'},radius = 25).add_to(m1)
+hm2 = HeatMap(dat,min_opacity=0.5,max_opacity=0.8,gradient={0.0: 'blue',  1.0: 'white'},radius = 25).add_to(m1)
 # st_folium(m1)
 
 # ,gradient={0.0: 'lightblue',  1.0: 'red'}
@@ -74,5 +74,15 @@ with col4:
     # st_folium(m2, use_container_width=True)
     folium_static(m2, width=650, height=650)
     st.write("Predicted Heatmap for rich localities")
+
+m3=folium.Map(location=[data_1.latitude.mean(),data_1.longitude.mean()],zoom_start=8,control_scale=True)
+# m1.fit_bounds([[27, 74], [31, 79]])
+map_values1 = data_1[['latitude','longitude','label']]
+map_values0 = data_0[['latitude','longitude','label']]
+dat1 = map_values1.values.tolist()
+dat0 = map_values0.values.tolist()
+hm3 = HeatMap(dat1,min_opacity=0.5,max_opacity=0.8,gradient={0.0: 'white',  1.0: 'red'},radius = 25).add_to(m3)
+hm4 = HeatMap(dat0,min_opacity=0.5,max_opacity=0.8,gradient={0.0: 'white',  1.0: 'red'},radius = 25).add_to(m3)
+st_folium(m3)
 # Run this with `streamlit run your_script_name.py`
 # Run this with `streamlit run your_script_name.py`
