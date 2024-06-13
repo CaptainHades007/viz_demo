9a568bf455be638047afa6ab16e93e0c241b6fc5
Pankaj Nandeshwar <106176661+CaptainHades007@users.noreply.github.com>
2024-06-13 09:38:48 +0530

    Update names.py

--- datasets/names.py
+++ datasets/names.py
@@ -5,4 +5,5 @@ INDORE_DATA_2 = './datasets/data/indore7km_all_pred_imp.csv'
 HARYANA_DATA_0 = './datasets/data/haryana_all_pred.csv'
 HARYANA_DATA_1 = './datasets/data/haryana_test_pred_imp.csv'
 HARYANA_DATA_2 = './datasets/data/haryana_test_test_imp.csv'
-
+RWI_HARYANA_DATA_1 = './datasets/data/rwi_haryana.csv'
+RWI_INDORE_DATA_1 = './datasets/data/rwi_indore.csv'
