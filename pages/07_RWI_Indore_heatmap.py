0dffe1d4b6af97925e53a7895102b03b4d3fb92d
Pankaj Nandeshwar <106176661+CaptainHades007@users.noreply.github.com>
2024-06-13 09:41:39 +0530

    Create 07_RWI_Indore_heatmap.py

---
+++ pages/07_RWI_Indore_heatmap.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+import folium
+from folium.plugins import HeatMap
+from streamlit_folium import st_folium
+from streamlit_folium import folium_static
+@st.cache_data
+def load_data_1():
+    data = pd.read_csv(names.RWI_INDORE_DATA_1)
+    return data
+
+# Load data
+data = load_data_1()
+
+m1=folium.Map(location=[data.latitude.mean(),data.longitude.mean()],zoom_start=8,control_scale=True)
+m1.fit_bounds([[21, 74], [24, 77]])
+map_values = data[['latitude','longitude','label']]
+dat = map_values.values.tolist()
+hm = HeatMap(dat,min_opacity=0.2,max_opacity=0.8,radius = 20).add_to(m1)
+# st_folium(m1)
+folium_static(m1)
